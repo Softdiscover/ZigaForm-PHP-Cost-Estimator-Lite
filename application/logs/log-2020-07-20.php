4b9ae327eb786d1d14d57884490458ba4a040f89
Laranginha <info@softdiscover.com>
2020-07-20 20:37:21 -0500

    Version 4.8.5

---
+++ application/logs/log-2020-07-20.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2020-07-20 23:48:26 --> 404 Page Not Found --> default/error
+ERROR - 2020-07-20 23:48:32 --> 404 Page Not Found --> default/error
